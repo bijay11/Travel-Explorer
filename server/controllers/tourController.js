f57c75009dc11066f521b1e15ebb407024f21cce
Bijay Tamang <tmg.bijay11@gmail.com>
2023-02-21 08:38:39 -0500

    add query to let the user filter the data

--- server/controllers/tourController.js
+++ server/controllers/tourController.js
@@ -2,7 +2,20 @@ const Tour = require('../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    const queryObj = { ...req.query };
+
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
+
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    queryStr = JSON.parse(queryStr);
+
+    const query = Tour.find(queryStr);
+
+    // EXECUTE QUERY
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
