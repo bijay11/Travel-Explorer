4c7fa59045f4a23ee554fcd7f84e5b047891f49f
Bijay Tamang <tmg.bijay11@gmail.com>
2023-02-23 23:07:35 -0500

    seperate error message for dev and prod

--- server/controllers/errorController.js
+++ server/controllers/errorController.js
@@ -1,8 +1,37 @@
+const sendErrorForDev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorForProd = (err, res) => {
+  // Operational, trusted error
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+
+  // Programming or other unknown error - so do not leak error details..
+  console.error('Error', err);
+
+  // Send Generic error message
+  return res.status(500).json({
+    status: 'error',
+    message: 'Something went wrong!',
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    return sendErrorForDev(err, res);
+  }
+  return sendErrorForProd(err, res);
 };
