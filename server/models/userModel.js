8b84676523de384a3a4c9ad72f46100e9cfb638c
Bijay Tamang <tmg.bijay11@gmail.com>
2023-02-25 08:14:11 -0500

    create userschema and User model

---
+++ server/models/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const { Schema, model } = mongoose;
+const { isEmail } = validator;
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Name  is required.'],
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: [isEmail, 'Please provide a valid email.'],
+    required: [true, 'Email is required.'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    minLength: 8,
+    required: [true, 'Please provide a password'],
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+User = model('User', userSchema);
+
+module.exports = User;
