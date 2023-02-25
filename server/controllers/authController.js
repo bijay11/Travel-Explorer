fa4b3dce5c6d7fa60222dc17ff34769a381b1441
Bijay Tamang <tmg.bijay11@gmail.com>
2023-02-25 12:17:21 -0500

    amend signup to only consume required fields from req.body

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,9 @@ const User = require('../models/userModel');
 const catchAsyncError = require('../helpers/catchAsyncError');
 
 exports.signup = catchAsyncError(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  // just filter what is required to signIn from req.body
+  const { name, email, password, passwordConfirm } = req.body;
+  const newUser = await User.create({ name, email, password, passwordConfirm });
 
   res.status(201).json({
     status: 'success',
