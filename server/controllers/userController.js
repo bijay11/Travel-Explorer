60755d6b14a8a551914252b5779a6d2621cacfc1
Bijay Tamang <tmg.bijay11@gmail.com>
2023-02-27 09:22:30 -0500

    add updateMe endpoint and endpoint collection.

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,5 +1,15 @@
 const User = require('../models/userModel');
 const catchAsyncError = require('../helpers/catchAsyncError');
+const AppError = require('../helpers/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
 
 exports.checkID = (req, res, next, val) => {
   // placeholder for the validation
@@ -15,6 +25,32 @@ exports.getAllUsers = catchAsyncError(async (req, res, next) => {
     data: { users },
   });
 });
+
+exports.updateMe = catchAsyncError(async (req, res, next) => {
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(new AppError('You cannot update password here.', 400));
+  }
+
+  // filter out what is required from req.body
+  // so that the user won't be able to pass unwanted values in DB
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // findById will end up using save method and it will trigger password validation as well
+  // so use findByIdAndUpdate for insenstive data
+  const updatedUser = await User.findByIdAndUpdate(req.user._id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  // Update user document
+  res.status(200).json({
+    status: 'success',
+    data: {
+      updatedUser,
+    },
+  });
+});
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
