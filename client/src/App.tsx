9caa47f35c23d4a344e3de9ce9ddd7c30d572ef9
Bijay Tamang <tmg.bijay11@gmail.com>
2023-03-05 18:51:44 -0500

    add hot reload option and sample pages to test it works

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
 import "./styles.css";
 import Image from "./React-icon.png";
+import { ClickCounter } from "./clickCounter";
 
 export const App = () => (
   <>
     <h1>
-      React component - {process.env.NODE_ENV} {process.env.name}
+      React component - {process.env.NODE_ENV}
+      {process.env.name}
     </h1>
     <img src={Image} alt="React Logo" width="300" height="200" />
+    <ClickCounter />
   </>
 );
--- client/webpack/webpack.dev.js
+++ client/webpack/webpack.dev.js
@@ -1,7 +1,14 @@
 const webpack = require("webpack");
 
+// An EXPERIMENTAL Webpack plugin to enable "Fast Refresh" (also known as Hot Reloading) for React components.
+const ReactRefreshWebpackPlugin = require("@pmmmwh/react-refresh-webpack-plugin");
+
 module.exports = {
   mode: "development",
+  devServer: {
+    hot: true,
+    open: true,
+  },
 
   // create-react-app recommendation for dev mode
   devtool: "cheap-module-source-map",
@@ -9,5 +16,8 @@ module.exports = {
     new webpack.DefinePlugin({
       "process.env.name": JSON.stringify("test-dev"),
     }),
+
+    //
+    new ReactRefreshWebpackPlugin(),
   ],
 };
